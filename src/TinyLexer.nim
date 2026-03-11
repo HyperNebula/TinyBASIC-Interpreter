2c41d2618feb244f4a75862e36a8f0378619b31d
HyperNebula <36902158+HyperNebula@users.noreply.github.com>
2026-03-11 13:19:07 -0600

    Added token constructor procedures

--- src/TinyLexer.nim
+++ src/TinyLexer.nim
@@ -4,10 +4,14 @@ type
     Lexer* = object
         source: string
         lineCount: int = 0
+        tokens: seq[Token]
 
 proc setSource*(lexer: var Lexer, source: string) =
     lexer.source = source
 
 proc print*(lexer: var Lexer) =
     lexer.lineCount.inc
-    echo $lexer.linecount, ": ", lexer.source
+
+    let sampleToken: Token = newToken(lexer.lineCount, STRING, lexer.source)
+
+    echo $lexer.linecount, ": ", $sampleToken
--- src/Token.nim
+++ src/Token.nim
@@ -1,10 +1,10 @@
 type
-    TokenType* = enum
+    TokenType* = enum ## Different possible token types
         PRINT, IF, GOTO, INPUT, LET, GOSUB, RETURN, CLEAR, LIST, RUN, END
         PLUS, MINUS, TIMES, DIVIDE, LESS, GREATER, EQUAL, COMMA
         VAR, DIGIT, STRING
         EOF
-    Token* = object
+    Token* = object ## Token object that stores relevant info
         lineNum: int
         case tokenType: TokenType
         of VAR:
@@ -16,16 +16,31 @@ type
         else:
             discard
 
+## Generic newToken procedure that creates and returns a token
+proc newToken* (lineNum: int, tokenType: TokenType): Token =
+    return Token(lineNum: lineNum, tokenType: tokenType)
+
+## String type newToken procedure that creates and returns either a VAR or STRING token
+proc newToken* (lineNum: int, tokenType: TokenType, value: string): Token =
+    if tokenType == VAR:
+        return Token(lineNum: lineNum, tokenType: VAR, name: value)
+    else:
+        return Token(lineNum: lineNum, tokenType: STRING, STRvalue: value)
+
+## Integer type newToken procecdure that creates and returns a DIGIT token
+proc newToken* (lineNum: int, tokenType: TokenType, INTvalue: int): Token =
+    return Token(lineNum: lineNum, tokenType: DIGIT, INTvalue: INTvalue)
+
 proc `$`* (token: Token): string =
-    var content: string
+    var content: string = ": "
 
     case token.tokenType:
     of VAR:
-        content = token.name
+        content.add token.name
     of DIGIT:
-        content = $token.INTvalue
+        content.add $token.INTvalue
     of STRING:
-        content = token.STRvalue
+        content.add token.STRvalue
     else:
         content = ""
     return "[" & $token.lineNum & "] " & $token.tokenType & content
